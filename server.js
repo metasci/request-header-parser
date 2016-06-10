ac4a656b0712a17e8f06bb4d80dd600f991c7ae1
Nick Beaudoin <believewithyoureyes@gmail.com>
2016-06-10 16:31:36 +0000

    begin app

---
+++ server.js
@@ -0,0 +1,15 @@
+
+var express = require('express');
+
+var app = express();
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + "/public/index.html");
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function(){
+  console.log("listening on port " + port + "...");
+});
